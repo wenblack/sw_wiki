85dd3c68c7d1dd5ae452c19cd799101a46c5c0b2
wenblack <wenblack012@gmail.com>
2022-12-13 16:17:21 -0300

    Final changes on Box components

--- components/Box/index.tsx
+++ components/Box/index.tsx
@@ -2,9 +2,9 @@ import { SVG } from "../SVG"
 
 export function Box() {
     return (
-        <div id="box" className=" border border-border rounded-md gap-14 items-center justify-center  p-5 mb-4 flex h-full w-5/6 max-w-3xl mt-20 ">
+        <div id="box" className=" border border-border rounded-md gap-14 items-center justify-around  p-5 mb-4 flex h-full w-5/6 max-w-3xl mt-20 ">
             <figure aria-label="Star Wars Logo">
-                <SVG height={'75px'} width={'75px'} />
+                <SVG height={'85px'} width={'85px'} />
             </figure>
             <div id="Container text" className="flex flex-items-start text-left flex-col">
                 <h1 id="title" className="font-bold text-link">SW WIKI</h1>
